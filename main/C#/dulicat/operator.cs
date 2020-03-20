9117126cd4b863fa8a8cbd578c5b3a77339e5ed8
Alex Romaniuk <alaster.rom@gmail.com>
2020-03-20 18:25:51 +0200

    3 file dublicate

---
+++ main/C#/dulicat/operator.cs
@@ -0,0 +1,55 @@
+using System;
+class Sample
+{
+   public static void Main() 
+   {
+      try{
+          int a=5,b=2;
+          double c = 19.67, d=3;
+          Console.WriteLine("Результат {0}%{1}={2}",a,b,a%b);
+          Console.WriteLine("Результат {0}%{1}={2}",c,d,c%d);
+         }
+      catch(Exception ex){
+          Console.WriteLine(ex.Message);
+         }
+			
+		
+      Console.Read();
+   }
+}
+
+{
+   public static void Main() 
+   {
+      try{
+          int a=5,b=2;
+          double c = 19.67, d=3;
+          Console.WriteLine("Результат {0}%{1}={2}",a,b,a%b);
+          Console.WriteLine("Результат {0}%{1}={2}",c,d,c%d);
+         }
+      catch(Exception ex){
+          Console.WriteLine(ex.Message);
+         }
+			
+		
+      Console.Read();
+   }
+}
+
+{
+   public static void Main() 
+   {
+      try{
+          int a=5,b=2;
+          double c = 19.67, d=3;
+          Console.WriteLine("Результат {0}%{1}={2}",a,b,a%b);
+          Console.WriteLine("Результат {0}%{1}={2}",c,d,c%d);
+         }
+      catch(Exception ex){
+          Console.WriteLine(ex.Message);
+         }
+			
+		
+      Console.Read();
+   }
+}
