6860abdf07ac8a30235faf39c33b6a678f90ac0d
Alex Romaniuk <alaster.rom@gmail.com>
2020-03-20 18:08:32 +0200

    3 file c#

---
+++ main/C#/first program.cs
@@ -0,0 +1,14 @@
+// Пространство имен
+namespace CSharpApplication.HelloWorld
+{
+   // Класс
+   class FirstApplication
+   {
+      // Точка входа в программу
+      static void Main()
+      {
+         // Вывод предложения на экран
+         System.Console.WriteLine("Привет, C#!\n");
+      }
+   }
+}
